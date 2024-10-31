1a7212911a2a8b27a5305c2bc0180fbecd677055
Chris <chris@theintertubes.ca>
2024-10-31 11:06:09 -0400

    added scaling, order of operations

--- cluster.tf
+++ cluster.tf
@@ -14,7 +14,7 @@ data "talos_machine_configuration" "machineconfig_cp" {
 }
 
 resource "talos_machine_configuration_apply" "cp_config_apply" {
-  depends_on                  = [ proxmox_virtual_environment_vm.talos_cp_01 ]
+  depends_on                  = [proxmox_virtual_environment_vm.talos_cp_01]
   client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
   machine_configuration_input = data.talos_machine_configuration.machineconfig_cp.machine_configuration
   count                       = 1
@@ -29,51 +29,45 @@ data "talos_machine_configuration" "machineconfig_worker" {
 }
 
 resource "talos_machine_configuration_apply" "worker_config_apply" {
-  depends_on                  = [ proxmox_virtual_environment_vm.talos_worker_01 ]
+  for_each = { for worker in local.workers : worker.name => worker }
+
   client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
   machine_configuration_input = data.talos_machine_configuration.machineconfig_worker.machine_configuration
-  count                       = 1
-  node                        = var.talos_worker_01_ip_addr
+  node                        = each.value.ip_address  # Referencing ip_address in each worker configuration
 }
 
-resource "talos_machine_configuration_apply" "worker2_config_apply" {
-  depends_on                  = [ proxmox_virtual_environment_vm.talos_worker_02 ]
-  client_configuration        = talos_machine_secrets.machine_secrets.client_configuration
-  machine_configuration_input = data.talos_machine_configuration.machineconfig_worker.machine_configuration
-  count                       = 1
-  node                        = var.talos_worker_02_ip_addr
-}
 resource "talos_machine_bootstrap" "bootstrap" {
-  depends_on           = [ talos_machine_configuration_apply.cp_config_apply ]
+  depends_on           = [talos_machine_configuration_apply.cp_config_apply]
   client_configuration = talos_machine_secrets.machine_secrets.client_configuration
   node                 = var.talos_cp_01_ip_addr
 }
 
 data "talos_cluster_health" "health" {
-  depends_on           = [ talos_machine_configuration_apply.cp_config_apply, talos_machine_configuration_apply.worker_config_apply ]
+  depends_on           = [talos_machine_configuration_apply.cp_config_apply, talos_machine_configuration_apply.worker_config_apply]
   client_configuration = data.talos_client_configuration.talosconfig.client_configuration
-  control_plane_nodes  = [ var.talos_cp_01_ip_addr ]
-  worker_nodes         = [ var.talos_worker_01_ip_addr, var.talos_worker_02_ip_addr ]
+  control_plane_nodes  = [var.talos_cp_01_ip_addr]
+  
+  # Use the IP addresses from local.workers to dynamically list worker node IPs
+  worker_nodes         = [for worker in local.workers : worker.ip_address]
+  
   endpoints            = data.talos_client_configuration.talosconfig.endpoints
 }
 
 data "talos_cluster_kubeconfig" "kubeconfig" {
-  depends_on           = [ talos_machine_bootstrap.bootstrap, data.talos_cluster_health.health ]
+  depends_on           = [talos_machine_bootstrap.bootstrap, data.talos_cluster_health.health]
   client_configuration = talos_machine_secrets.machine_secrets.client_configuration
   node                 = var.talos_cp_01_ip_addr
 }
 
 output "talosconfig" {
-  value = data.talos_client_configuration.talosconfig.talos_config
+  value     = data.talos_client_configuration.talosconfig.talos_config
   sensitive = true
 }
 
 output "kubeconfig" {
-  value = data.talos_cluster_kubeconfig.kubeconfig.kubeconfig_raw
+  value     = data.talos_cluster_kubeconfig.kubeconfig.kubeconfig_raw
   sensitive = true
 }
-
-####
 
 resource "local_file" "kubeconfig_file" {
   content  = data.talos_cluster_kubeconfig.kubeconfig.kubeconfig_raw
